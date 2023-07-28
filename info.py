8f6f01e90709562ea93de24296e2863cf5ce51bb
Pruthvi6943 <134065804+Pruthvi6943@users.noreply.github.com>
2023-07-28 23:41:22 +0530

    Update info.py

--- info.py
+++ info.py
@@ -18,7 +18,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '6075536616:AAHhCHPr5fmqhCdxVK2CUEciOxzMi7g
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
-PICS = (environ.get('PICS', 'https://graph.org/file/41221d13c9b161ff306f6.jpg')).split()
+PICS = (environ.get('PICS', 'https://graph.org/file/7f0eac247ec95aaebcd7a.jpg')).split()
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '969099516, 1758343727').split()]
