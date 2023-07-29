d9043e0028caa507cd9216104d7d524939aacac9
Pruthvi6943 <134065804+Pruthvi6943@users.noreply.github.com>
2023-07-29 14:55:59 +0530

    Update check_alive.py

--- plugins/check_alive.py
+++ plugins/check_alive.py
@@ -14,15 +14,15 @@ async def help(_, message):
 
 @Client.on_message(filters.command("credits", CMD))
 async def credits(_, message):
-    await message.reply_text("Tʜɪs Is Cᴏᴅᴇᴅ Bʏ @CSAdmin69_bot/n/Tʜᴀɴᴋs Tᴏ Eᴠᴀ Mᴀʀɪᴇ ﹝ ʙᴀsᴇ ᴄᴏᴅᴇ ")
+    await message.reply_text("Tʜɪs Is Cᴏᴅᴇᴅ Bʏ @𝗖𝗦𝗔𝗗𝗠𝗜𝗡𝟲𝟵_𝗕𝗢𝗧/n/Tʜᴀɴᴋs Tᴏ Eᴠᴀ Mᴀʀɪᴇ ﹝ ʙᴀsᴇ ᴄᴏᴅᴇ ")
 
 @Client.on_message(filters.command("movies", CMD))
 async def movie(_, message):
-    await message.reply_text("⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯\n𝗠𝗼𝘃𝗶𝗲𝘀 𝗥𝗲𝗾𝘂𝗲𝘀𝘁 𝗙𝗼𝗿𝗺𝗮𝘁\n⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯\n\nGO TO GOOGLE ➠ TYPE MOVIE NAME ➠ COPY MOVIE NAME FROM GOOGLE ➠ PASTE COPIED MOVIE NAME IN THE BOT OR REQUEST GROUP\n\nEXAMPLE : SAMAJAVARAGAMANA 2023 ELA NAME AND YEAR PETANDI\n\n🚯 DONT TYPE IN THIS FORMAT 🤧 ➠ :(LANGUAGE MENTION CHEYAKANDII,AND MAIN THING THEATRE PRINTS ARE NOT UPLOADED IN OUR BOT⚠️⚠️)\n\nCODED BY @CSADMIN69_BOT")
+    await message.reply_text("⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯\n𝗠𝗼𝘃𝗶𝗲𝘀 𝗥𝗲𝗾𝘂𝗲𝘀𝘁 𝗙𝗼𝗿𝗺𝗮𝘁\n⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯\n\nɢᴏ ᴛᴏ ɢᴏᴏɢʟᴇ ➠ ᴛʏᴘᴇ ᴍᴏᴠɪᴇ ɴᴀᴍᴇ ➠ ᴄᴏᴘʏ ᴍᴏᴠɪᴇ ɴᴀᴍᴇ ғʀᴏᴍ ɢᴏᴏɢʟᴇ ➠ ᴘᴀsᴛᴇ ᴄᴏᴘɪᴇᴅ ᴍᴏᴠɪᴇ ɴᴀᴍᴇ ɪɴ ᴛʜᴇ ʙᴏᴛ ᴏʀ ʀᴇǫᴜᴇsᴛ ɢʀᴏᴜᴘ\n\nᴇxᴀᴍᴘʟᴇ : sᴀᴍᴀᴊᴀᴠᴀʀᴀɢᴀᴍᴀɴᴀ 2023 ᴇʟᴀ ɴᴀᴍᴇ ᴀɴᴅ ʏᴇᴀʀ ᴘᴇᴛᴀɴᴅɪ\n\n🚯 ᴅᴏɴᴛ ᴛʏᴘᴇ ɪɴ ᴛʜɪs ғᴏʀᴍᴀᴛ 🤧 ➠ :(ʟᴀɴɢᴜᴀɢᴇ ᴍᴇɴᴛɪᴏɴ ᴄʜᴇʏᴀᴋᴀɴᴅɪɪ,ᴀɴᴅ ᴍᴀɪɴ ᴛʜɪɴɢ ᴛʜᴇᴀᴛʀᴇ ᴘʀɪɴᴛs ᴀʀᴇ ɴᴏᴛ ᴜᴘʟᴏᴀᴅᴇᴅ ɪɴ ᴏᴜʀ ʙᴏᴛ⚠️⚠️))\n\nᴄᴏᴅᴇᴅ ʙʏ @𝗖𝗦𝗔𝗗𝗠𝗜𝗡𝟲𝟵_𝗕𝗢𝗧")
 
 @Client.on_message(filters.command("series", CMD))
 async def series(_, message):
-    await message.reply_text("⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯\n𝗦𝗲𝗿𝗶𝗲𝘀 𝗥𝗲𝗾𝘂𝗲𝘀𝘁 𝗙𝗼𝗿𝗺𝗮𝘁\n⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯\n\nGO TO GOOGLE ➠ TYPE SERIES NAME ➠ COPY SERIES NAME ADD SEASON NUMBER LIKE S01 BESIDE MOVIE NAME ➠ PASTE COPIED SERIES NAME WITH SEASON IN THE BOT OR REQUEST GROUP\n\nᴇxᴀᴍᴘʟᴇ : ᴍᴏɴᴇʏ ʜᴇɪsᴛ S01E01\n\n🚯 DONT TYPE IN THIS FORMAT ➠ ':(Money Heist season 1,ELA TYPE CHESTHE RAVU PARTICULAR FORMAT LO PAMPALI PAINA FORMAT CHUDANDI )\n\nCODED BY @CSADMIN69_BOT")
+    await message.reply_text("⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯\n𝗦𝗲𝗿𝗶𝗲𝘀 𝗥𝗲𝗾𝘂𝗲𝘀𝘁 𝗙𝗼𝗿𝗺𝗮𝘁\n⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯⋯\n\nɢᴏ ᴛᴏ ɢᴏᴏɢʟᴇ ➠ ᴛʏᴘᴇ sᴇʀɪᴇs ɴᴀᴍᴇ ➠ ᴄᴏᴘʏ sᴇʀɪᴇs ɴᴀᴍᴇ ᴀᴅᴅ sᴇᴀsᴏɴ ɴᴜᴍʙᴇʀ ʟɪᴋᴇ s01 ʙᴇsɪᴅᴇ ᴍᴏᴠɪᴇ ɴᴀᴍᴇ ➠ ᴘᴀsᴛᴇ ᴄᴏᴘɪᴇᴅ sᴇʀɪᴇs ɴᴀᴍᴇ ᴡɪᴛʜ sᴇᴀsᴏɴ ɪɴ ᴛʜᴇ ʙᴏᴛ ᴏʀ ʀᴇǫᴜᴇsᴛ ɢʀᴏᴜᴘ\n\nᴇxᴀᴍᴘʟᴇ : ᴍᴏɴᴇʏ ʜᴇɪsᴛ s01ᴇ01\n\n🙅‍♂️🙅‍♂️ᴅᴏɴᴛ ᴛʏᴘᴇ ɪɴ ᴛʜɪs ғᴏʀᴍᴀᴛ ➠ ':(ᴍᴏɴᴇʏ ʜᴇɪsᴛ sᴇᴀsᴏɴ 1,ᴇʟᴀ ᴛʏᴘᴇ ᴄʜᴇsᴛʜᴇ ʀᴀᴠᴜ ᴘᴀʀᴛɪᴄᴜʟᴀʀ ғᴏʀᴍᴀᴛ ʟᴏ ᴘᴀᴍᴘᴀʟɪ ᴘᴀɪɴᴀ ғᴏʀᴍᴀᴛ ᴄʜᴜᴅᴀɴᴅɪ ) )\n\nᴄᴏᴅᴇᴅ ʙʏ @𝗖𝗦𝗔𝗗𝗠𝗜𝗡𝟲𝟵_𝗕𝗢𝗧")
 
 @Client.on_message(filters.command("download", CMD))
 async def tutorial(_, message):
