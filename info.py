2971c3cc90ed159c7f61a51fb54ab0e59031cf8e
ARRIPAPPA <137713659+ARRIPAPPA@users.noreply.github.com>
2023-09-19 19:09:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://BLASTER:BLASTER@cluster0.mtxmw0b.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://AUTO:AUTO@cluster0.3kadqge.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
